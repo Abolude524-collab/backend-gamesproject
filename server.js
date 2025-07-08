b32ab5bb75e87010e50a37e66c94ad7c9e3813db
Abolude524 <testimonyabolude@gmail.com>
2025-07-09 00:33:07 +0100

    Fix: corrected port

--- server.js
+++ server.js
@@ -143,7 +143,8 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = 4000;
+
+const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
-  console.log(`Tic Tac Toe server listening on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
